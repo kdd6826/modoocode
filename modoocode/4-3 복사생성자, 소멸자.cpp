6da3ac66b73776665d01559597f1ccae4a1753ba
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-01-09 13:48:06 +0900

    4-3 복사생성자,소멸자 完

--- modoocode/4-3 복사생성자, 소멸자.cpp
+++ modoocode/4-3 복사생성자, 소멸자.cpp
@@ -334,15 +334,81 @@
 //}
 
 
+//class Photon_Cannon {
+//	int hp, shield;
+//	int coord_x, coord_y;
+//	int damage;
+//
+//	char *name;
+//public:
+//	Photon_Cannon(int x, int y);
+//	Photon_Cannon(int x, int y ,const char *cannon_name);
+//	~Photon_Cannon();
+//
+//	void show_status();
+//};
+//
+//Photon_Cannon::Photon_Cannon(int x, int y)
+//{
+//	hp = shield = 100;
+//	coord_x = x;
+//	coord_y = y;
+//	damage = 20;
+//
+//	name = NULL;
+//}
+//
+//Photon_Cannon::Photon_Cannon(int x, int y, const char* cannon_name)
+//{
+//	hp = shield = 100;
+//	coord_x = x;
+//	coord_y = y;
+//	damage = 20;
+//
+//	name = new char[strlen(cannon_name) + 1];
+//	strcpy_s(name, strlen(cannon_name) + 1, cannon_name);
+//}
+//
+//
+//
+//Photon_Cannon::~Photon_Cannon()
+//{
+//	if (name) delete[] name;
+//}
+//
+//void Photon_Cannon::show_status()
+//{
+//	std::cout << "Photon Cannon " << std::endl;
+//	std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) " << std::endl;
+//	std::cout << " HP : " << hp << std::endl;
+//}
+//
+//int main() {
+//	Photon_Cannon pc1(3, 3, "Cannon");
+//	Photon_Cannon pc2 = pc1;
+//	/*
+//		복사 생성자의 위험성.
+//		name의 경우 소멸자 때 "Cannon"을 delete 한다.
+//		그러한 경우 pc2가 가리키던 "Cannon" 또한 delete가되고,
+//		pc2의 name은 이미 해제된(pc1이 먼저) 상태가된다.
+//		이는 깊은 복사로 해줄 수 있다.
+//	*/
+//
+//
+//	pc1.show_status();
+//	pc2.show_status();
+//}
+
 class Photon_Cannon {
 	int hp, shield;
 	int coord_x, coord_y;
 	int damage;
 
-	char *name;
+	char* name;
 public:
 	Photon_Cannon(int x, int y);
-	Photon_Cannon(int x, int y ,const char *cannon_name);
+	Photon_Cannon(int x, int y, const char* cannon_name);
+	Photon_Cannon(const Photon_Cannon &pc);
 	~Photon_Cannon();
 
 	void show_status();
@@ -354,6 +420,8 @@ Photon_Cannon::Photon_Cannon(int x, int y)
 	coord_x = x;
 	coord_y = y;
 	damage = 20;
+
+	name = NULL;
 }
 
 Photon_Cannon::Photon_Cannon(int x, int y, const char* cannon_name)
@@ -367,7 +435,18 @@ Photon_Cannon::Photon_Cannon(int x, int y, const char* cannon_name)
 	strcpy_s(name, strlen(cannon_name) + 1, cannon_name);
 }
 
+Photon_Cannon::Photon_Cannon(const Photon_Cannon& pc)
+{
+	std::cout << "복사 생성자 호출! " << std::endl;
+	hp = pc.hp;
+	shield = pc.shield;
+	coord_x = pc.coord_x;
+	coord_y = pc.coord_y;
+	damage = pc.damage;
 
+	name = new char[strlen(pc.name) + 1];
+	strcpy_s(name, strlen(pc.name) + 1, pc.name);
+}
 
 Photon_Cannon::~Photon_Cannon()
 {
@@ -382,9 +461,15 @@ void Photon_Cannon::show_status()
 }
 
 int main() {
-	Photon_Cannon pc1(3, 3);
+	Photon_Cannon pc1(3, 3, "Cannon");
 	Photon_Cannon pc2 = pc1;
-	
+	/*
+		복사 생성자의 위험성.
+		name의 경우 소멸자 때 "Cannon"을 delete 한다.
+		그러한 경우 pc2가 가리키던 "Cannon" 또한 delete가되고,
+		pc2의 name은 이미 해제된(pc1이 먼저) 상태가된다.
+		이는 깊은 복사로 해줄 수 있다.
+	*/
 
 
 	pc1.show_status();
