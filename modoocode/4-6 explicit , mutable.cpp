9cbea350808f0b71a10be9e08246701921800bf0
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-01-11 22:34:44 +0900

    시프 시작 커밋

---
+++ modoocode/4-6 explicit , mutable.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+class MyString {
+	char* string_content;
+	int string_length;
+
+	int memory_capacity;
+
+public:
+	explicit MyString(int capacity);
+
+	MyString(const char* str);
+
+	MyString(const MyString& str);
+
+	~MyString();
+
+	int length();
+};
+
+
+
+MyString::MyString(int capacity)
+{
+	string_content = new char[capacity];
+	string_length = 0;
+	memory_capacity = capacity;
+	std::cout << "Capacity : " << capacity << std::endl;
+}
+
+MyString::MyString(const char* str)
+{
+	string_length = 0;
+	while (str[string_length++]){
+	}
+	string_content = new char[string_length];
+	memory_capacity = string_length;
+
+	for (int i = 0; i != string_length; i++) string_content[i] = str[i];
+}
+
+MyString::MyString(const MyString& str)
+{
+	string_length = str.string_length;
+	string_content = new char[string_length];
+
+	for (int i = 0; i != string_length; i++)
+	{
+		string_content[i] = str.string_content[i];
+	}
+}
+
+MyString::~MyString()
+{
+	delete[] string_content;
+}
+
+int MyString::length()
+{
+	return string_length;
+}
+
+void DoSomethingWithString(MyString s) {
+
+}
+
+int main()
+{
+	//DoSomethingWithString(3);
+}
