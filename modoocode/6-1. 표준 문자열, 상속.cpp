cf5823756e32d2aba61b184f26086427d5829729
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-01-20 00:05:18 +0900

    6-1 中

--- modoocode/6-1. 표준 문자열, 상속.cpp
+++ modoocode/6-1. 표준 문자열, 상속.cpp
@@ -52,3 +52,53 @@ public:
 	}
 	int calculate_pay() { return 200 + rank * 50; }
 };
+
+class EmployeeList {
+	int alloc_employee;
+	int current_employee;
+	Employee** employee_list;
+
+public:
+	EmployeeList(int alloc_employee) :alloc_employee(alloc_employee) {
+		employee_list = new Employee * [alloc_employee];
+		current_employee = 0;
+	}
+	void add_employee(Employee* employee) {
+		employee_list[current_employee] = employee;
+		current_employee++;
+	}
+	int current_employee_num() { return current_employee; }
+
+	void print_employee_info() {
+		int total_pay = 0;
+		for (int i = 0; i < current_employee; i++) {
+			employee_list[i]->print_info();
+			total_pay += employee_list[i]->calculate_pay();
+		}
+		std::cout << "총 비용 : " << total_pay << "만원 " << std::endl;
+	}
+	~EmployeeList() {
+		for (int i = 0; i < current_employee; i++) {
+			delete employee_list[i];
+		}
+		delete[] employee_list;
+	}
+};
+
+int main() {
+	EmployeeList emp_list(10);
+	emp_list.add_employee(new Employee("노홍철", 34, "평사원", 1));
+	emp_list.add_employee(new Employee("하하", 34, "평사원", 1));
+
+	emp_list.add_employee(new Employee("유재석", 41, "부장", 7));
+	emp_list.add_employee(new Employee("정준하", 43, "과장", 4));
+	emp_list.add_employee(new Employee("박명수", 43, "차장", 5));
+	emp_list.add_employee(new Employee("정형돈", 36, "대리", 2));
+	emp_list.add_employee(new Employee("길", 36, "인턴", -2));
+	emp_list.print_employee_info();
+	return 0;
+}
+
+class Manager {
+
+};
