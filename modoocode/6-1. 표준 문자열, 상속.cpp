8e7a42847b227d387994c8be48cc896ffed9c232
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-01-21 21:36:47 +0900

    6-1 中

--- modoocode/6-1. 표준 문자열, 상속.cpp
+++ modoocode/6-1. 표준 문자열, 상속.cpp
@@ -25,114 +25,139 @@
 //
 //	return 0;
 //}
+//
+//class Employee {
+//	std::string name;
+//	int age;
+//	
+//	std::string position;
+//	int rank;
+//
+//public:
+//	Employee(std::string name, int age, std::string position, int rank)
+//		: name(name), age(age), position(position), rank(rank) {}
+//
+//	Employee(const Employee & employee) {
+//		name = employee.name;
+//		age = employee.age;
+//		position = employee.position;
+//		rank = employee.rank;
+//	}
+//
+//	Employee() {}
+//
+//	void print_info() {
+//		std::cout << name << " (" << position << " , " << age << ") ==> "
+//			<< calculate_pay() << "만원" << std::endl;
+//	}
+//	int calculate_pay() { return 200 + rank * 50; }
+//};
+//
+//class EmployeeList {
+//	int alloc_employee;
+//
+//	int current_employee;
+//	int current_manager;
+//
+//	Employee** employee_list;
+//	Manager** manager_list;
+//public:
+//	EmployeeList(int alloc_employee) :alloc_employee(alloc_employee) {
+//		employee_list = new Employee * [alloc_employee];
+//		current_employee = 0;
+//	}
+//	void add_employee(Employee* employee) {
+//		employee_list[current_employee] = employee;
+//		current_employee++;
+//	}
+//	int current_employee_num() { return current_employee; }
+//
+//	void print_employee_info() {
+//		int total_pay = 0;
+//		for (int i = 0; i < current_employee; i++) {
+//			employee_list[i]->print_info();
+//			total_pay += employee_list[i]->calculate_pay();
+//		}
+//		std::cout << "총 비용 : " << total_pay << "만원 " << std::endl;
+//	}
+//	~EmployeeList() {
+//		for (int i = 0; i < current_employee; i++) {
+//			delete employee_list[i];
+//		}
+//		delete[] employee_list;
+//	}
+//};
+//
+//int main() {
+//	EmployeeList emp_list(10);
+//	emp_list.add_employee(new Employee("노홍철", 34, "평사원", 1));
+//	emp_list.add_employee(new Employee("하하", 34, "평사원", 1));
+//
+//	emp_list.add_employee(new Employee("유재석", 41, "부장", 7));
+//	emp_list.add_employee(new Employee("정준하", 43, "과장", 4));
+//	emp_list.add_employee(new Employee("박명수", 43, "차장", 5));
+//	emp_list.add_employee(new Employee("정형돈", 36, "대리", 2));
+//	emp_list.add_employee(new Employee("길", 36, "인턴", -2));
+//	emp_list.print_employee_info();
+//	return 0;
+//}
+//
+//class Manager {
+//	std::string name;
+//	int age;
+//
+//	std::string position;
+//	int rank;
+//	int year_of_service;
+//
+//public:
+//	Manager(std::string name, int age, std::string position, int rank,
+//		int year_of_service)
+//		: year_of_service(year_of_service),
+//		name(name),
+//		age(age),
+//		position(position),
+//		rank(rank) {}
+//
+//	Manager(const Manager& manager) {
+//		name = manager.name;
+//		age = manager.age;
+//		position = manager.position;
+//		rank = manager.rank;
+//		year_of_service = manager.year_of_service;
+//	}
+//
+//	Manager() {}
+//
+//	int calcuate_pay() { return 200 + rank * 50 + 5 * year_of_service; }
+//	void print_info() {
+//		std::cout << name << " (" << position << " , " << age << ", "
+//			<< year_of_service << "년차) ==> " << calcuate_pay() << "만원"
+//			<< std::endl;
+//	}
+//};
 
-class Employee {
-	std::string name;
-	int age;
-	
-	std::string position;
-	int rank;
+class Base {
+	std::string s;
+public:
+	Base() : s("기반") { std::cout << "기반 클래스" << std::endl; }
+
+	void what() { std::cout << s << std::endl; }
+};
+class Derived : public Base {
+	std::string s;
 
 public:
-	Employee(std::string name, int age, std::string position, int rank)
-		: name(name), age(age), position(position), rank(rank) {}
-
-	Employee(const Employee & employee) {
-		name = employee.name;
-		age = employee.age;
-		position = employee.position;
-		rank = employee.rank;
-	}
-
-	Employee() {}
-
-	void print_info() {
-		std::cout << name << " (" << position << " , " << age << ") ==> "
-			<< calculate_pay() << "만원" << std::endl;
-	}
-	int calculate_pay() { return 200 + rank * 50; }
-};
-
-class EmployeeList {
-	int alloc_employee;
-
-	int current_employee;
-	int current_manager;
-
-	Employee** employee_list;
-	Manager** manager_list;
-public:
-	EmployeeList(int alloc_employee) :alloc_employee(alloc_employee) {
-		employee_list = new Employee * [alloc_employee];
-		current_employee = 0;
-	}
-	void add_employee(Employee* employee) {
-		employee_list[current_employee] = employee;
-		current_employee++;
-	}
-	int current_employee_num() { return current_employee; }
-
-	void print_employee_info() {
-		int total_pay = 0;
-		for (int i = 0; i < current_employee; i++) {
-			employee_list[i]->print_info();
-			total_pay += employee_list[i]->calculate_pay();
-		}
-		std::cout << "총 비용 : " << total_pay << "만원 " << std::endl;
-	}
-	~EmployeeList() {
-		for (int i = 0; i < current_employee; i++) {
-			delete employee_list[i];
-		}
-		delete[] employee_list;
+	Derived() :Base(), s("파생") {
+		std::cout << "파생 클래스" << std::endl;
+		what();
 	}
 };
-
 int main() {
-	EmployeeList emp_list(10);
-	emp_list.add_employee(new Employee("노홍철", 34, "평사원", 1));
-	emp_list.add_employee(new Employee("하하", 34, "평사원", 1));
+	std::cout << " === 기반 클래스 생성 ===" << std::endl;
+	Base p;
+	std::cout << " === 파생 클래스 생성 ===" << std::endl;
+	Derived c;
 
-	emp_list.add_employee(new Employee("유재석", 41, "부장", 7));
-	emp_list.add_employee(new Employee("정준하", 43, "과장", 4));
-	emp_list.add_employee(new Employee("박명수", 43, "차장", 5));
-	emp_list.add_employee(new Employee("정형돈", 36, "대리", 2));
-	emp_list.add_employee(new Employee("길", 36, "인턴", -2));
-	emp_list.print_employee_info();
 	return 0;
 }
-
-class Manager {
-	std::string name;
-	int age;
-
-	std::string position;
-	int rank;
-	int year_of_service;
-
-public:
-	Manager(std::string name, int age, std::string position, int rank,
-		int year_of_service)
-		: year_of_service(year_of_service),
-		name(name),
-		age(age),
-		position(position),
-		rank(rank) {}
-
-	Manager(const Manager& manager) {
-		name = manager.name;
-		age = manager.age;
-		position = manager.position;
-		rank = manager.rank;
-		year_of_service = manager.year_of_service;
-	}
-
-	Manager() {}
-
-	int calcuate_pay() { return 200 + rank * 50 + 5 * year_of_service; }
-	void print_info() {
-		std::cout << name << " (" << position << " , " << age << ", "
-			<< year_of_service << "년차) ==> " << calcuate_pay() << "만원"
-			<< std::endl;
-	}
-};
