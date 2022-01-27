96072378c296ca65251ae8d650cccf3968e4aa0a
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-01-27 23:29:23 +0900

    7-2 中 // 유니티로

--- modoocode/7-2 파일 입출력.cpp
+++ modoocode/7-2 파일 입출력.cpp
@@ -2,13 +2,54 @@
 #include <iostream>
 #include <string>
 
+//int main() {
+//	std::ifstream in("test.txt");
+//	std::string s;
+//
+//	if (in.is_open()) {
+//		in >> s;
+//		std::cout << "입력 받은 문자열 :: " << s << std::endl;
+//	}
+//	else {
+//		std::cout << "파일을 찾을 수 없습니다!" << std::endl;
+//	}
+//	return 0;
+//}
+
+//int main() {
+//	std::ifstream in("test.txt");
+//	std::string s;
+//
+//	if (in.is_open()) {
+//		in >> s;
+//		std::cout << "입력 받은 문자열 :: " << s << std::endl;
+//	}
+//	else {
+//		std::cout << "파일을 찾을 수 없습니다!" << std::endl;
+//	}
+//
+//	in.close();
+//	in.open("other.txt");
+//
+//	if (in.is_open()) {
+//		in >> s;
+//		std::cout << "입력 받은 문자열 :: " << s << std::endl;
+//	}
+//	else {
+//		std::cout << "파일을 찾을 수 없습니다!" << std::endl;
+//	}
+//
+//	return 0;
+//}
+
 int main() {
-	std::ifstream in("test.txt");
+	std::ifstream in("test.txt", std::ios::binary);
 	std::string s;
 
+	int x;
 	if (in.is_open()) {
-		in >> s;
-		std::cout << "입력 받은 문자열 :: " << s << std::endl;
+		in.read((char*)(&x), 4);
+		std::cout << std::hex << x << std::endl;
 	}
 	else {
 		std::cout << "파일을 찾을 수 없습니다!" << std::endl;
